747967ba9a782115735f36d0c483ff96c346db43
Dawud Ibrahim Ismail <ismaildawud96@gmail.com>
2021-09-11 16:13:42 +0000

    run tests on master push or pr

--- src/main/java/tech/idawud/bloggy/MainVerticle.java
+++ src/main/java/tech/idawud/bloggy/MainVerticle.java
@@ -2,22 +2,24 @@ package tech.idawud.bloggy;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class MainVerticle extends AbstractVerticle {
 
-  @Override
-  public void start(Promise<Void> startPromise) throws Exception {
-    vertx.createHttpServer().requestHandler(req -> {
-      req.response()
-        .putHeader("content-type", "text/plain")
-        .end("Hello from Vert.x!");
-    }).listen(8888, http -> {
-      if (http.succeeded()) {
-        startPromise.complete();
-        System.out.println("HTTP server started on port 8888");
-      } else {
-        startPromise.fail(http.cause());
-      }
-    });
-  }
+	private static final int HTTP_PORT = Integer.parseInt(System.getenv().getOrDefault("HTTP_PORT", "8888"));
+
+	@Override
+	public void start(Promise<Void> startPromise) throws Exception {
+		vertx.createHttpServer().requestHandler(req -> {
+			req.response().putHeader("content-type", "text/plain").end("Hello from Vert.x!");
+		}).listen(HTTP_PORT, http -> {
+			if (http.succeeded()) {
+				startPromise.complete();
+				log.info("HTTP server started on port {}", HTTP_PORT);
+			} else {
+				startPromise.fail(http.cause());
+			}
+		});
+	}
 }
--- src/test/java/tech/idawud/bloggy/TestMainVerticle.java
+++ src/test/java/tech/idawud/bloggy/TestMainVerticle.java
@@ -1,8 +1,14 @@
 package tech.idawud.bloggy;
 
 import io.vertx.core.Vertx;
+import io.vertx.ext.web.client.WebClient;
+import io.vertx.ext.web.codec.BodyCodec;
 import io.vertx.junit5.VertxExtension;
 import io.vertx.junit5.VertxTestContext;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -10,13 +16,33 @@ import org.junit.jupiter.api.extension.ExtendWith;
 @ExtendWith(VertxExtension.class)
 public class TestMainVerticle {
 
-  @BeforeEach
-  void deploy_verticle(Vertx vertx, VertxTestContext testContext) {
-    vertx.deployVerticle(new MainVerticle(), testContext.succeeding(id -> testContext.completeNow()));
-  }
+	private static final String LOCALHOST = "localhost";
+	private static final int HTTP_PORT = Integer.parseInt(System.getenv().getOrDefault("HTTP_PORT", "8888"));
 
-  @Test
-  void verticle_deployed(Vertx vertx, VertxTestContext testContext) throws Throwable {
-    testContext.completeNow();
-  }
+	@BeforeEach
+	void deploy_verticle(Vertx vertx, VertxTestContext testContext) {
+		vertx.deployVerticle(new MainVerticle(), testContext.succeeding(id -> testContext.completeNow()));
+	}
+
+	@Test
+	void verticle_deployed(Vertx vertx, VertxTestContext testContext) throws Throwable {
+		testContext.completeNow();
+	}
+
+	@Test
+	void http_server_response_check(Vertx vertx, VertxTestContext testContext) {
+		WebClient webClient = WebClient.create(vertx);
+		final String expectedBody = "Hello from Vert.x!";
+		final String expectedContentTypeHeader = "text/plain";
+
+		webClient.get(HTTP_PORT, LOCALHOST, "/").as(BodyCodec.string()).send(testContext.succeeding(response -> {
+			testContext.verify(() -> {
+				assertEquals(expectedBody, response.body());
+				assertNotNull(response.getHeader("content-type"));
+				assertEquals(expectedContentTypeHeader, response.getHeader("content-type"));
+				testContext.completeNow();
+			});
+		}));
+	}
+
 }
